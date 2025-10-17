569ce1b93ce9db8b3ab249a61fb8dc5343c8c13b
Do Minh Thang <dominhthang101004@gmail.com>
2025-10-17 14:41:34 +0700

    thêm folder admin và trang inventory

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Layout from "./components/layout/Layout";
 import useUserStore from "./store/userStore";
 import OrdersPage from "./pages/orders/OrdersPage";
+import InventoryPage from "./pages/AdminViews/InventoryPage";
 
 export default function App() {
   const { user } = useUserStore();
@@ -19,6 +20,7 @@ export default function App() {
         element={user ? <Layout><Dashboard /></Layout> : <Navigate to="/login" />}
       />
       <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/inventory" element={<InventoryPage />} />
     </Routes>
   );
 }
